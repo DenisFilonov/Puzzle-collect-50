3ce275215a0ab78a363808d378076dc0de42f3e2
denisfilonov <dean.mitz6@gmail.com>
2022-12-10 18:22:41 +0100

    Add project files.

---
+++ WindowsFormsApp5_pd/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp5_pd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chbDigits = new System.Windows.Forms.CheckedListBox();
+            this.txbDigits = new System.Windows.Forms.TextBox();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // chbDigits
+            // 
+            this.chbDigits.CheckOnClick = true;
+            this.chbDigits.FormattingEnabled = true;
+            this.chbDigits.Items.AddRange(new object[] {
+            "25",
+            "27",
+            "3",
+            "12",
+            "6",
+            "16",
+            "9",
+            "30",
+            "21",
+            "19"});
+            this.chbDigits.Location = new System.Drawing.Point(28, 23);
+            this.chbDigits.Name = "chbDigits";
+            this.chbDigits.Size = new System.Drawing.Size(90, 191);
+            this.chbDigits.TabIndex = 0;
+            this.chbDigits.SelectedIndexChanged += new System.EventHandler(this.chbDigits_SelectedIndexChanged);
+            // 
+            // txbDigits
+            // 
+            this.txbDigits.Location = new System.Drawing.Point(178, 23);
+            this.txbDigits.Multiline = true;
+            this.txbDigits.Name = "txbDigits";
+            this.txbDigits.ReadOnly = true;
+            this.txbDigits.Size = new System.Drawing.Size(102, 144);
+            this.txbDigits.TabIndex = 1;
+            this.txbDigits.Text = "Подбери числа";
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(175, 182);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(53, 16);
+            this.lblSum.TabIndex = 2;
+            this.lblSum.Text = "Сумма:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 321);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.txbDigits);
+            this.Controls.Add(this.chbDigits);
+            this.Name = "Form1";
+            this.Text = "Собери полтос";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox chbDigits;
+        private System.Windows.Forms.TextBox txbDigits;
+        private System.Windows.Forms.Label lblSum;
+    }
+}
+
---
+++ WindowsFormsApp5_pd/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5_pd
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chbDigits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if (chbDigits.CheckedItems != null)
+            //{
+            //    txbDigits.Text += chbDigits.Text + "\r\n";
+            //}
+            int sum = 0;
+
+            txbDigits.Text = "";
+            if (chbDigits.CheckedItems.Count == 0)
+            {
+                txbDigits.Text = "";
+
+            }
+            var list = chbDigits.CheckedItems;
+
+            foreach (var item in list)
+            {
+                txbDigits.Text += item + Environment.NewLine;
+                sum += int.Parse(item.ToString());
+            }
+            
+            list = null;
+            lblSum.Text = "Сумма: " + sum.ToString();
+
+            if (sum == 50)
+            {
+                if (MessageBox.Show("Вы успешно решили головоломку", "Поздравляю", MessageBoxButtons.OK,MessageBoxIcon.Information) == DialogResult.OK)
+                {
+                    for (int i = 0; i < chbDigits.Items.Count; i++)
+                    {
+                        chbDigits.SetItemChecked(i, false);
+                    }
+                    txbDigits.Text = "Подбери числа";
+                    lblSum.Text = "Сумма: ";
+                }
+                MessageBox.Show("Игра обнулена", "Отлично", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
